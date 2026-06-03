bac8be0764ff02bab3a432a4c08c2596300e463c
Nguyen Pham Viet Duc <phamvietduc231st@gmail.com>
2026-06-03 11:19:06 +0700

    fix: eager-load comments in PostSearch to resolve N+1 query leak

--- models/search/PostSearch.php
+++ models/search/PostSearch.php
@@ -43,7 +43,7 @@ class PostSearch extends Post
      */
     public function search($params, $formName = null)
     {
-        $query = Post::find()->notDeleted()->with('media');
+        $query = Post::find()->notDeleted()->with(['media', 'comments']);
 
 
 
